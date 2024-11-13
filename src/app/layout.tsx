77852946379683e28c75bcc0864d7c03a10e00b9
Bhanu Thakur <bhanu@Bhanus-MacBook-Pro.local>
2024-11-13 21:32:37 +0000

    initial code commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Flowbite",
+  description: "Flowbite - a app built with Next.js and Tailwind CSS.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn(poppins.className, "antialiased")}>{children}</body>
+    </html>
+  );
+}
